a19176b06e91b2520d71e2ff6c5181d877dbdf42
chetan.dawkhar <chetan.dawkhar@harbingergroup.com>
2019-09-10 11:07:04 +0530

    added test case

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class DemoTestCase(TestCase):
+    def test_add_two_number_valid(self):
+        a = 10
+        b = 20
+        c = a + b
+        self.assertEqual(c, 30)
+
+    # def test_add_two_number_invalid(self):
+    #     a = 10
+    #     b = 20
+    #     c = a + b
+    #     self.assertEqual(c, 40)
